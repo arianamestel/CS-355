0328d48008595c17691f9ae74bc228c079af043c
arianamestel-cbi <ariana.mestel@cbinsights.com>
2019-09-07 22:57:17 -0400

    trying to deploy

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/views'));
+
+app.set("view options", {layout: false});
+app.use(express.static('./'));
+
+app.get('/', function(req, res) {
+	res.render("index.html");
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
