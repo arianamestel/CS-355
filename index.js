eed9e43ee5548c84687990455a82b639e289449d
josephsalomon <josephsalomon321@gmail.com>
2019-12-05 16:39:04 -0500

    this isnt working

--- index.js
+++ index.js
@@ -10,72 +10,76 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-app.set("view options", {layout: false});
+app.set("view options", {
+  layout: false
+});
 app.use(express.static('./'));
 
 
-app.get('/', function(req, res) {
-	res.render("index");
+app.get('/', function (req, res) {
+  res.render("index");
 });
 
-app.get('/about-me', function(req, res) {
-	res.render("about/aboutMe");
+app.get('/about-me', function (req, res) {
+  res.render("about/aboutMe");
 });
 
-app.get('/contact-us', function(req, res) {
-	res.render("contact/contactUs");
+app.get('/contact-us', function (req, res) {
+  res.render("contact/contactUs");
 });
 
-app.get('/contact-success', function(req, res) {
+app.get('/contact-success', function (req, res) {
   res.render("contact/contactSuccess");
 });
 
-app.get("/browser", function(req, res) {
+app.get("/browser", function (req, res) {
   res.render("browser/browser");
 });
 
-app.get("/navigator", function(req, res) {
-	res.render("browser/navigator");
+app.get("/navigator", function (req, res) {
+  res.render("browser/navigator");
 });
 
-app.get("/window", function(req, res) {
+app.get("/window", function (req, res) {
   res.render("browser/window");
 });
 
-app.get("/screen", function(req, res) {
+app.get("/screen", function (req, res) {
   res.render("browser/screen");
 });
 
-app.get("/location", function(req, res) {
+app.get("/location", function (req, res) {
   res.render("browser/location");
 });
 
-app.get("/geolocation", function(req, res) {
+app.get("/geolocation", function (req, res) {
   res.render("browser/geolocation");
 });
 
-app.get("/from-file", function(req, res) {
+app.get("/from-file", function (req, res) {
   res.render("search/fromFile");
 });
 
-app.get("/google-api", function(req, res) {
+app.get("/google-api", function (req, res) {
   res.render("search/googleApi");
 });
 
-app.get("/index-url", function(req, res) {
+app.get("/index-url", function (req, res) {
   res.render("admin/indexer");
 });
 
-app.get("/my-search-engine", function(req, res) {
+app.get("/my-search-engine", function (req, res) {
   res.render("search/mySearchEngine");
 });
 
-app.post("/my-search-engine", function(req, res) {
+app.post("/my-search-engine", function (req, res) {
   var searchTerm = req.body.searchTerm;
   var searchDate = new Date();
   var start = new Date().getTime();
@@ -89,111 +93,180 @@ app.post("/my-search-engine", function(req, res) {
   // saveSearchTerm(searchTerm, numResults, timeToSearch, dateSearched)
 });
 
-app.get("/history-stats", function(req, res) {
+app.get("/history-stats", function (req, res) {
   res.render("admin/history-stats");
 });
 
-app.post("/index-url",  function(req, res) {
+app.post("/index-url", function (req, res) {
   var url = req.body.url;
-  var links = indexMainURL(url);  
+  var links = indexMainURL(url);
 });
 
 
 function indexMainURL(url) {
   // this function gets all words and all links
+
+
   var start = new Date().getTime();
-  var date = new Date();
+  
   axios.get(url)
     .then(response => {
       var $ = cheerio.load(response.data);
       var words = getWords(response.data);
       getLinks(response.data);
-      var end = new Date().getTime(); 
+      var end = new Date().getTime();
       var seconds = ((end - start) / 1000).toFixed(2);
-
+      
+      var date;
+      date = new Date()
+      date = date.getUTCFullYear() + '-' +
+      ('00' + (date.getUTCMonth()+1)).slice(-2) + '-' +
+      ('00' + date.getUTCDate()).slice(-2) + ' ' + 
+      ('00' + date.getUTCHours()).slice(-2) + ':' + 
+      ('00' + date.getUTCMinutes()).slice(-2) + ':' + 
+      ('00' + date.getUTCSeconds()).slice(-2);
       // get urls info
       var linkInfo = {
         "title": $("title").text(),
         "url": url,
         "description": $('meta[name="description"]').attr('content'),
         "lastModified": $('meta[name="last-modifed"]').attr('content'), // get last modified (it in headers somewhere)
-        "lastIndexed": date, // get the data of last time it was indexed
+        "lastIndexed": date,
+       // get the data of last time it was indexed
         "timeToIndex": seconds // record the amount of time it took to index
       }
 
-         //TODO fix the datetimes from NOW() to actual datetimes, also need to fix time to index,
-      
+      //TODO fix the datetimes from NOW() to actual datetimes, also need to fix time to index,
+
       saveLink(linkInfo)
-      
+
       saveWords(words, linkInfo)
-        
-       console.log(linkInfo);
+
+      console.log(linkInfo);
     })
     .catch(error => {
       console.log(error);
     });
 }
 
-function saveWords(wordArray, linkInfo){
-  var pageId ={};
-     mysqlConnection.query(
-     "SELECT pageId FROM page WHERE url = '"+linkInfo.url+"';",
-     function(err, rows, fields){
-       if(err) console.log(err);
-       var count = rows.length;
-         if(count == 1){
-       pageId= rows[0].pageId;
-       console.log("got PAGEID"+ pageId);
-         }
-         else console.log(" CANT GET PAGE ID FOR SOME REASON");
+function saveWords(wordArray, linkInfo) {
+  var pageId = {};
+  mysqlConnection.query(
+    "SELECT pageId FROM page WHERE url = '" + linkInfo.url + "';",
+    function (err, rows, fields) {
+      if (err) console.log(err);
+      var count = rows.length;
+      if (count == 1) {
+        pageId = rows[0].pageId;
+        console.log("got PAGEID" + pageId);
+      } else console.log(" CANT GET PAGE ID FOR SOME REASON");
+      //  console.log(wordArray[1]);
+    })
+  for (let i = 0; i < wordArray.length; i++) {
+
+    wordId = {};
+   
+        
         //  console.log(wordArray[1]);
-        })
-         for(let i=0;i<wordArray.length;i++){
-         
-           wordId = {};
-         mysqlConnection.query("SELECT wordId FROM word WHERE wordName=?" , wordArray[i],
-         function(err, rows){
+      
+/*
+HOW TO CONSTRUCT IF STATEMENTS (pseudo code):
 
-         if(err) console.log(err);
-         
-        //  if(rows){
-        //  wordId = rows.wordId
-        //  console.log("GOT WORD ID added" + wordId);
-        //  }
-        //  else {
-           console.log("WORD IS NOT IN DB PLEASE ADD: " +wordArray[i])
-           mysqlConnection.query(
-             "INSERT INTO word (wordName) VALUES ('"+wordArray[i]+"')", 
-             function(err, result){
-               if(err) {console.log(err);}
-               else{
-               console.log("1 word record added to word: "+wordArray[i]);
-               }
-             }
-           );
+if word exists in words
+  if words exists in page_word
+    update pageword to freq = freq +1
+  else 
+    insert into page_word  wordID pageId freq =1
+else 
+  insert into word, worf
+  insert into page_word pageID wordId freq = 1
+
+*/
+// mysqlConnection.query(
+//   "SELECT wordId FROM word WHERE wordName = '" + wordArray[i] + "';",
+//   function (err, rows, fields) {
+//     if (err) console.log(err);
+//     wordId = rows[0].wordId;
+//   });
+        mysqlConnection.query(
+          "CASE "+
+            "WHEN ((SELECT count(pageId) FROM page_word WHERE pageId = \""+pageId+"\" AND wordId = ?) > 0) "+
+                "THEN UPDATE page_word SET freq = freq+1 WHERE pageId =\""+pageId+"\" AND wordId = ? "+
+            "WHEN ((SELECT count(wordName) FROM word WHERE wordName = \""+wordArray[i]+"\") >0)"+
+              "THEN INSERT INTO page_word (pageId, wordId, freq) VALUES (\""+pageId+"\", ? , 1)"+
+              "ELSE "+
+          "BEGIN "+
+            "INSERT INTO word (wordName) VALUES (\""+wordArray[i]+"\") "+
+            "INSERT INTO page_word (pageId, wordId, freq) VALUES (\""+pageId+"\", ? , 1)"+
+          " END"
+          // "IF (SELECT count(wordName) FROM word WHERE wordName = \""+wordArray[i]+"\" >0)"+
+          // "THEN BEGIN"+
+          //   "IF (SELECT count(pageId) FROM page_word WHERE pageId = \""+pageId+"\" AND wordId = ? > 0)"+//wordId+//figure out later how to get that
+          //   "THEN BEGIN"+
+          //     "UPDATE page_word SET freq = freq+1 WHERE pageId =\""+pageId+"\" AND wordId = ?"+//wordId+//figure out later how to get that
+          //   "END"+
+          //   "ELSE"+
+          //   "BEGIN"+
+          //     "INSERT INTO page_word (pageId, wordId, freq) VALUES (\""+pageId+"\", ? , 1)"+
+          //   "END END IF"+
+          // "END"+
+          // "ELSE"+
+          // "BEGIN"+
+          //   "INSERT INTO word (wordName) VALUES (\""+wordArray[i]+"\")"+
+          //   "INSERT INTO page_word (pageId, wordId, freq) VALUES (\""+pageId+"\", ? , 1)"+
+          // "END END IF"
+          ,[getWordId(wordArray[i]), getWordId(wordArray[i]), getWordId(wordArray[i]), getWordId(wordArray[i])],
+          function (err, result) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("1 word record added to word: " + wordArray[i]);
+            }
+          }
+        );
         //  }
 
-       });
-         }
-         
-     
-     
+      // });
+  }
+
+// mysqlConnection.query("SELECT wordId FROM word WHERE wordName = \""+wordArray[i]+"\"", 
+//             function(err, result){
+//             if(err) console.log(err);
+//             })[0].wordId
+
 
 }
-function saveLink(linkInfo){
+function getWordId(words){
+    let exists = {}
+  try{ return mysqlConnection.query("IF (SELECT wordId FROM word WHERE wordName = \""+words+"\"", 
+            function(err, result){
+            if(err) console.log(err);
+            })[0].wordId;
+  }
+  catch(err){
+    console.log(" error cant find word porb "+ err)
+    return null;
+  }
+  return null;
+           
+
+}
+
+function saveLink(linkInfo) {
   // linkInfo.description = linkInfo.description.replace(/\/g,"'");
   //     linkInfo.url = linkInfo.url.replace(/\/g,"'");
   //     linkInfo.title = linkInfo.title.replace(/\/g,"'");
-         //TODO fix the datetimes from NOW() to actual datetimes, also need to fix time to index,
-      mysqlConnection.query("INSERT INTO page (title, url, description, lastModified, lastIndexed, timeToIndex) VALUES (\""+linkInfo.title+"\", \""+linkInfo.url+"\", \""
-      +linkInfo.description+"\", NOW(), NOW(), 12)", 
-      function(err, result){
-        if(err) console.log(err);
-        console.log("1 link/page record added")
-        
-      })
-}
+  //TODO fix the datetimes from NOW() to actual datetimes, also need to fix time to index,
+  mysqlConnection.query( 
+   "INSERT INTO page (title, url, description, lastModified, lastIndexed, timeToIndex) VALUES (\"" + linkInfo.title + "\", \"" + linkInfo.url + "\", \"" +
+    linkInfo.description + "\", NOW(), \""+linkInfo.lastIndexed+"\", \""+linkInfo.timeToIndex+"\") " ,
+    function (err, result) {
+      if (err) console.log(err);
+      console.log("1 link/page record added")
 
+    })
+}
+// \""+linkInfo.lastModified+"\",
 
 
 function getWords(html) {
@@ -209,7 +282,7 @@ function getLinks(html) {
   var $ = cheerio.load(html);
   var a = $("a");
   var links = [];
-  $(a).each(function(i, link) {
+  $(a).each(function (i, link) {
     // push the link into the link array and then index that link
     if (validURL($(link).attr('href'))) {
       links.push(indexLink($(link).attr('href')));
@@ -224,7 +297,16 @@ function indexLink(link) {
   var date = new Date();
   axios.get(link)
     .then(response => {
-      var end = new Date().getTime(); 
+      var date;
+      date = new Date();
+      date = date.getUTCFullYear() + '-' +
+      ('00' + (date.getUTCMonth()+1)).slice(-2) + '-' +
+      ('00' + date.getUTCDate()).slice(-2) + ' ' + 
+      ('00' + date.getUTCHours()).slice(-2) + ':' + 
+      ('00' + date.getUTCMinutes()).slice(-2) + ':' + 
+      ('00' + date.getUTCSeconds()).slice(-2);
+
+      var end = new Date().getTime();
       var seconds = ((end - start) / 1000).toFixed(2);
       var $ = cheerio.load(response.data);
       // get words from link
@@ -234,20 +316,20 @@ function indexLink(link) {
         "title": $("title").text(),
         "url": link,
         "description": $('meta[name="description"]').attr('content'),
-        "lastModified": $('meta[name="last-modifed"]').attr('content'),// get last modified
+        "lastModified": $('meta[name="last-modifed"]').attr('content'), // get last modified
         "lastIndexed": date, // get the data of last time it was indexed
         "timeToIndex": seconds // record the amount of time it took to index
       }
-    
-    
+
+
       saveLink(linkInfo)
-      saveWords(words,linkInfo);
-     
+      saveWords(words, linkInfo);
+
       console.log(linkInfo);
       return linkInfo;
 
     })
-    
+
     .catch(error => {
       console.log(error);
     });
@@ -267,16 +349,18 @@ var mysqlConnection = mysql.createConnection({
   multipleStatements: true
 });
 
-mysqlConnection.connect((err)=>{
-  if(!err){
+mysqlConnection.connect((err) => {
+  if (!err) {
     console.log("Connected to the database!");
-  }
-  else{
+  } else {
     console.log("connection to database Failed :(");
   }
 });
 
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+app.listen(port, function () {
+  console.log('Our app is running on http://localhost:' + port);
 });
+
+
+
